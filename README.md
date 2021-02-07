c9f19725849734926c98bb238e51d2a80e7894b8
namako11 <mr.logavatu@gmail.com>
2021-02-07 02:06:09 -0800

    Added to Comparative function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # myProjects
- Projects I work on to practice coding. 
+
+Projects I work on to practice coding. 
+
+The first project is a numberGuesser game written in javaScript.
+The project is part of my course in Codecademy.
---
+++ number-guesser-starting/script.js
@@ -0,0 +1,46 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+/*
+Create a generateTarget() function. This function will be called at the start of each new round in order to generate the new secret target number.
+
+This function should return a random integer between 0 and 9.
+*/
+const generateTarget = () =>
+    console.log Math.floor(Math.random() * 10 + 1);
+
+    
+
+/* 
+Create a compareGuesses() function. 
+This function will be called each round to determine which guess is closest to the target number.
+
+This function:
+* Has three parameters representing the user (human) guess, a computer guess, and the secret target number to be guessed.
+* Determines which player (human or computer) wins based on which guess is closest to the target. 
+* If both players are tied, the human user should win.
+* Return true if the human player wins, and false if the computer player wins.
+*/
+
+const compareGuesses = (userInput, computer, target) => {
+    let target = generateTarget();
+    let computer = generateTarget();
+    let userInput = 0;
+    let winMsg = "You Win!"
+    let losMsg = "Computer Wins"
+
+    if (userInput < 0 || userInput > 9) {
+        return 'Error! Enter a valid number.' // fail user input
+    } else if { 
+        return userInput;
+    } else if (Math.abs(userInput - target) < Maths.abs(computer - target)) {
+        return winMsg;
+    } else if (Math.abs(computer - target) < Maths.abs(userInput - target)) {
+        return losMsg;
+    } else ((userInput === target) & (computer === target)) {
+        return winMsg;
+    }
+}
